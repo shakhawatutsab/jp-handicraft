15b03b5cd8425b67fbe320d33fc3f05c0a6f19b5
utsab <shakhawatutsab@gmail.com>
2026-08-19 14:18:44 +0600

    config shop.php

---
+++ config/shop.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'phone' => env('SHOP_PHONE', '+8801700000000'),
+    'phone_display' => env('SHOP_PHONE_DISPLAY', '01700-000000'),
+    'whatsapp' => env('SHOP_WHATSAPP', '8801700000000'),
+    'facebook' => env('SHOP_FACEBOOK', 'https://facebook.com/your-page'),
+];
